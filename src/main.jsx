b21208eeb407b17e5cf0b1478991e31787c5c284
Piyush Kanojiya <raat131221@gmail.com>
2026-08-30 23:40:51 +0530

    feat: mount global CFlow theme toggle

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/theme.css'
 import App from './App.jsx'
+import ThemeToggle from './components/ThemeToggle.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <ThemeToggle />
+  </>,
+)
